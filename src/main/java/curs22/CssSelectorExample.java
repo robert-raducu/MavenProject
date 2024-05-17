86491d419e968792d0e6db6f7ccc49465154d9f8
Robert <robert_raducu@yahoo.com>
2024-05-17 18:04:57 +0300

    curs 22 final

--- src/main/java/curs22/CssSelectorExample.java
+++ src/main/java/curs22/CssSelectorExample.java
@@ -3,13 +3,14 @@ package curs22;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 import selenium.utils.BaseTest;
 
 public class CssSelectorExample extends BaseTest{
 
-	@Test
+	//@Test
 	public void cssExample1() {
 		JavascriptExecutor js = (JavascriptExecutor)driver;
 		
@@ -36,5 +37,69 @@ public class CssSelectorExample extends BaseTest{
 		
 		js.executeScript("arguments[0].setAttribute('style', 'background:yellow ;border:10px solid green')", 
 				submitButton);
+	}
+	
+	//@Test
+	public void cssSelectorExample2() {
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		
+		//first-of-type
+		WebElement newReleasesMenu = driver.findElement(By.cssSelector("ul[class*='sc_tabs_titles']"
+				+ ">li:first-of-type"));
+		js.executeScript("arguments[0].setAttribute('style', 'background:yellow ;border:10px solid green')", 
+				newReleasesMenu);
+		
+		//nth-of-type
+		
+		WebElement comingSoon = driver.findElement(By.cssSelector("ul[class*='sc_tabs_titles']"
+				+ ">li:nth-of-type(2)"));
+		js.executeScript("arguments[0].setAttribute('style', 'background:yellow ;border:10px solid green')", 
+				comingSoon);
+		
+		WebElement bestSellers = driver.findElement(By.cssSelector("ul[class*='sc_tabs_titles']"
+				+ ">li:nth-of-type(3)"));
+		js.executeScript("arguments[0].setAttribute('style', 'background:yellow ;border:10px solid green')", 
+				bestSellers);
+		
+		//last-of-type
+		WebElement awardWinners = driver.findElement(By.cssSelector("ul[class*='sc_tabs_titles']"
+				+ ">li:last-of-type"));
+		js.executeScript("arguments[0].setAttribute('style', 'background:blue ;border:10px solid red')", 
+				awardWinners);
+	}
+	
+	@Test
+	public void cssSelectorExample3() {
+		
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		driver.findElement(By.cssSelector("div[class='menu_main_wrap'] a[href*='shop/']")).click();
+		
+		Actions action = new Actions(driver);
+		action.scrollByAmount(0, 1250).perform();
+		
+		//first-child
+		WebElement category1 = driver.findElement(By.cssSelector("ul[class='product-categories']>li:first-child"));
+		
+		js.executeScript("arguments[0].setAttribute('style', 'background:blue ;border:10px solid red')", 
+				category1);
+		
+		
+		//last-child
+        WebElement category11 = driver.findElement(By.cssSelector("ul[class='product-categories']>li:last-child"));
+		
+		js.executeScript("arguments[0].setAttribute('style', 'background:blue ;border:10px solid red')", 
+				category11);
+		
+		
+        WebElement category4 = driver.findElement(By.cssSelector("ul[class='product-categories']>li:nth-child(4)"));
+		
+		js.executeScript("arguments[0].setAttribute('style', 'background:green ;border:10px solid yellow')", 
+				category4);
+		
+		//selectez din n in n (n=ce numar vreau eu)
+		//-->ul[class='product-categories']>li:nth-child(3n)
+		
+		//se porneste cu nr de la nr 5 si selectez din 3 in 3(adica acel 3n)
+		//ul[class='product-categories']>li:nth-child(3n+5)
 	}
 }
